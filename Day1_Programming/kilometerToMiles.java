1b1b093d08533d62dcf1ad62e82ec02df62fd358
Mayur Ramani <ramanimayur836@gmail.com>
2025-08-12 00:49:37 +0530

    Added kilometerToMiles Code

---
+++ Day1_Programming/kilometerToMiles.java
@@ -0,0 +1,7 @@
+public class KilometersToMiles {
+    public static void main(String[] args) {
+        double distanceInKM=10.8;
+        double distanceInMiles=distanceInKM/1.6;
+        System.out.println("The distance "+distanceInKM+"km in miles is "+distanceInMiles);
+    }
+}
