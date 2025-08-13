47721a4386e626c62a1db6242e8bc7bad17538c4
Mayur Ramani <ramanimayur836@gmail.com>
2025-08-13 11:36:37 +0530

    Added PositiveNegativeOrZero Code

---
+++ controlFlow/PositiveNegativeOrZero.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PositiveNegativeOrZero {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = in.nextInt();
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
