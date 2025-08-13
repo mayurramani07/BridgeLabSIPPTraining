360b1188ecdec18ebdc642dd58828ac859479f59
Mayur Ramani <ramanimayur836@gmail.com>
2025-08-13 11:17:14 +0530

    EvenOddArray code

--- ARRAYS/EvenOddArrays.java
+++ ARRAYS/EvenOddArrays.java
@@ -2,7 +2,6 @@ import java.util.*;
 public class EvenOddArrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int number = sc.nextInt();
 		if(number<=0) {
