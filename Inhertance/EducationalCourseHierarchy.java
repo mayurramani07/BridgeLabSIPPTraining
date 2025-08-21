aa5312973b5b4845fe9ad28ed02d2a70febdf35b
Mayur Ramani <ramanimayur836@gmail.com>
2025-08-21 12:09:00 +0530

    Added EducationalCorse

---
+++ Inhertance/EducationalCourseHierarchy.java
@@ -0,0 +1,42 @@
+class Course {
+    private String courseName;
+    private int duration; // in weeks
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+}
+
+class OnlineCourse extends Course {
+    private String platform;
+    private boolean isRecorded;
+
+    public OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+}
+
+class PaidOnlineCourse extends OnlineCourse {
+    private double fee;
+    private double discount;
+
+    public PaidOnlineCourse(String courseName, int duration, String platform, boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+
+    public void displayDetails() {
+        System.out.println("Paid Online Course: " + fee + " (Discount: " + discount + ")");
+    }
+}
+
+public class EducationalCourseHierarchy {
+    public static void main(String[] args) {
+        PaidOnlineCourse poc = new PaidOnlineCourse("Java Programming", 8, "Udemy", true, 1999, 20);
+        poc.displayDetails();
+    }
+}
