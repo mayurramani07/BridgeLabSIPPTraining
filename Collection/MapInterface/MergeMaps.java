818cdf201e66016b82b98521d30b2985fdc0b02d
Mayur Ramani <ramanimayur836@gmail.com>
2025-08-26 16:50:56 +0530

    [Mayur Ramani] Added MergeMaps Code

---
+++ Collection/MapInterface/MergeMaps.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MergeMaps {
+    public static void main(String[] args) {
+        Map<String, Integer> map1 = new HashMap<>();
+        map1.put("A", 1);
+        map1.put("B", 2);
+
+        Map<String, Integer> map2 = new HashMap<>();
+        map2.put("B", 3);
+        map2.put("C", 4);
+
+        for (Map.Entry<String, Integer> entry : map2.entrySet()) {
+            map1.merge(entry.getKey(), entry.getValue(), Integer::sum);
+        }
+
+        System.out.println("Merged Map: " + map1);
+    }
+}
