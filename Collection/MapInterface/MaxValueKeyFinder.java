cb7d328903c67c31a47e3e430f8c63508d74bc42
Mayur Ramani <ramanimayur836@gmail.com>
2025-08-26 16:51:58 +0530

    [Mayur Ramani] Added MaxValueKey Code

---
+++ Collection/MapInterface/MaxValueKeyFinder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MaxValueKeyFinder {
+    public static void main(String[] args) {
+        Map<String, Integer> map = Map.of("A", 10, "B", 20, "C", 15);
+        String maxKey = null;
+        int maxValue = Integer.MIN_VALUE;
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxValue) {
+                maxValue = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+
+        System.out.println("Key with max value: " + maxKey);
+    }
+}
