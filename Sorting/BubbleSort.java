f0616a31aac729bebfb77ddfb489159db3c1a549
Mayur Ramani <ramanimayur836@gmail.com>
2025-09-17 11:16:40 +0530

    [Mayur Ramani] Added BubbleSort Code

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        int[] marks = new int[n];
+        System.out.println("Enter student marks:");
+        for (int i = 0; i < n; i++) {
+            marks[i] = sc.nextInt();
+        }
+        bubbleSort(marks);
+        System.out.println("Sorted marks: " + Arrays.toString(marks));
+    }
+}
